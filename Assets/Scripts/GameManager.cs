cfeaa06c941e383a5770bb119eac2c31de2cc248
Chetan Baraiya <chetanbaraiya672@gmail.com>
2024-11-16 15:27:53 +0530

    setup player name

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,37 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-    
-    
+
+    public TMP_Text playerNameText;
     public TMP_Text msgText;
     public GameObject msgObject;
 
     void Awake()
-    {     
+    {
         if (Instance == null)
-        {       
+        {
             Instance = this;
             DontDestroyOnLoad(gameObject);
-        }    
-        else     
-        {       
+        }
+        else
+        {
             Destroy(gameObject);
-        }     
-    } 
+        }
+    }
 
-     public void ShowRechargeMessage()
+    private void Start()
+    {
+        playerNameText.text = PlayerPrefs.GetString("PlayerName");;
+    }
+
+    public void ShowRechargeMessage()
     {
         msgObject.SetActive(true);
         msgText.text = "Press R to reload the gun";
     }
-     
-     
-
-
 }
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,10 +1,17 @@
+using TMPro;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public TMP_InputField playerNameInput;
+
     public void OnClickStart()
     {
+        string name = playerNameInput.text;
+        PlayerPrefs.SetString("PlayerName",name);
+        SceneManager.LoadScene("Gameplay");
     }
 
     public void ClickOnQuit()
